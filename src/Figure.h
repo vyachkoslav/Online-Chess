0415957bdde585eb7c62a6dcb4751fad6e26ddf1
Vyacheslav Denisov <81619887+vyachkoslav@users.noreply.github.com>
2022-04-27 20:06:07 +0300

    Added Figure classes

--- src/Figure.h
+++ src/Figure.h
@@ -1,19 +1,30 @@
 #pragma once
 
-#include "pch.h"
+class Figure;
 
-enum class Side { white, black };
+#include "pch.h"
+#include "Game.h"
 
 class Figure {
 protected:
 	Side side;
 	std::string position;
 	std::string sprite;
+	unsigned moveCount = 0;
 
 public:
-	Side getSide() { return side; }
-	std::string getPosition() { return position; }
-	std::string getSprite() { return sprite; }
+	Figure(Side side) { this->side = side; }
+
+	Side getSide() const { return side; }
+	std::string getPosition() const { return position; }
+	std::string getSprite() const { return sprite; }
 
 	virtual std::vector<std::string> getAvailableMoves() = 0;
 };
+
+class Pawn : public Figure {};
+class Knight : public Figure {};
+class Bishop : public Figure {};
+class Rook : public Figure {};
+class Queen : public Figure {};
+class King : public Figure {};
