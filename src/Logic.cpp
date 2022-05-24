64aa82a7546ed24065f8ed12346873623a180122
Vyacheslav Denisov <81619887+vyachkoslav@users.noreply.github.com>
2022-05-24 16:00:20 +0300

    Fixed rook logic

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -3,13 +3,13 @@
 
 using VectorOfPairs = std::vector<std::pair<int, int>>;
 
-VectorOfPairs Logic::getAvailableMovesForFigure(std::pair<int, int> figPosition)
+VectorOfPairs Logic::getAvailableMovesForFigure(std::pair<int, int> figPos)
 {
-	int index = figPosition.first + figPosition.second * 8;
+	int index = figPos.first + figPos.second * 8;
 	const Figure* fig = (*board)[index];
 	switch (fig->name) {
 	case 'P':
-		return availableMovesForPawn(figPosition);
+		return availableMovesForPawn(figPos);
 	}
 }
 
@@ -32,13 +32,13 @@ int inline Logic::sgn(int val) {
 	return (0 < val) - (val < 0);
 }
 
-VectorOfPairs Logic::availableMovesForPawn(std::pair<int, int> figPosition) {
-	if (figPosition.second == 7 || figPosition.second == 0)
+VectorOfPairs Logic::availableMovesForPawn(std::pair<int, int> figPos) {
+	if (figPos.second == 7 || figPos.second == 0)
 		throw std::invalid_argument("Pawn shouldn't be at last row");
 	
 	std::vector<std::pair<int, int>> out;
 	
-	int index = pairToInd(figPosition.first, figPosition.second);
+	int index = pairToInd(figPos.first, figPos.second);
 	const Figure** fig = board[index];
 	const char& name = (*fig)->name;
 	int frontCell = std::isupper(name) ? 8 : -8;
@@ -51,28 +51,54 @@ VectorOfPairs Logic::availableMovesForPawn(std::pair<int, int> figPosition) {
 			out.push_back(indToPair(index + frontCell * 2));
 		}
 	}
-	if (figPosition.first != 0 && isEnemy(*fig, fig[frontCell - sgn(frontCell)])) { // not on edge and diag left is enemy
+	if (figPos.first != 0 && isEnemy(*fig, fig[frontCell - sgn(frontCell)])) { // not on edge and diag left is enemy
 		out.push_back(indToPair(index + frontCell - sgn(frontCell)));
 	}
-	if (figPosition.first != 7 && isEnemy(*fig, fig[frontCell + sgn(frontCell)])) { // not on edge and diag right is enemy
+	if (figPos.first != 7 && isEnemy(*fig, fig[frontCell + sgn(frontCell)])) { // not on edge and diag right is enemy
 		out.push_back(indToPair(index + frontCell + sgn(frontCell)));
 	}
 	return out;
 }
 
-VectorOfPairs Logic::availableMovesForRook(std::pair<int, int> figPosition) {
+VectorOfPairs Logic::availableMovesForRook(std::pair<int, int> figPos) {
 	std::vector<std::pair<int, int>> out;
-	for (int x = figPosition.first + 1; x < 8 && isEmpty(*board[pairToInd(x, figPosition.second)]); x++) {
-		out.push_back(std::pair<int, int>(x, figPosition.second));
+	const Figure* fig = *board[pairToInd(figPos)];
+
+	for (int x = figPos.first + 1; x < 8; x++) {					//right
+		if(isEmpty(*board[pairToInd(x, figPos.second)]))
+			out.push_back(std::pair<int, int>(x, figPos.second));
+		if (isEnemy(fig, *board[pairToInd(x, figPos.second)])) {
+			out.push_back(std::pair<int, int>(x, figPos.second));
+			break;
+		}
 	}
-	for (int x = figPosition.first - 1; x > 0 && isEmpty(*board[pairToInd(x, figPosition.second)]); x++) {
-		out.push_back(std::pair<int, int>(x, figPosition.second));
+
+	for (int x = figPos.first - 1; x > 0; x--) {					//left
+		if (isEmpty(*board[pairToInd(x, figPos.second)]))
+			out.push_back(std::pair<int, int>(x, figPos.second));
+		if (isEnemy(fig, *board[pairToInd(x, figPos.second)])) {
+			out.push_back(std::pair<int, int>(x, figPos.second));
+			break;
+		}
 	}
-	for (int y = figPosition.second + 1; y < 8 && isEmpty(*board[pairToInd(figPosition.first, y)]); y++) {
-		out.push_back(std::pair<int, int>(figPosition.first, y));
+
+	for (int y = figPos.second + 1; y < 8; y++) {					//down
+		if (isEmpty(*board[pairToInd(figPos.first, y)]))
+			out.push_back(std::pair<int, int>(figPos.first, y));
+		if (isEnemy(fig, *board[pairToInd(figPos.first, y)])){
+			out.push_back(std::pair<int, int>(figPos.first, y));
+			break;
+		}
 	}
-	for (int y = figPosition.second - 1; y > 0 && isEmpty(*board[pairToInd(figPosition.first, y)]); y++) {
-		out.push_back(std::pair<int, int>(figPosition.first, y));
+
+	for (int y = figPos.second - 1; y > 0; y--) {					//up
+		if (isEmpty(*board[pairToInd(figPos.first, y)]))
+			out.push_back(std::pair<int, int>(figPos.first, y));
+		if (isEnemy(fig, *board[pairToInd(figPos.first, y)])) {
+			out.push_back(std::pair<int, int>(figPos.first, y));
+			break;
+		}
 	}
+
 	return out;
 }
